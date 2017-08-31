9e86cec85ada01e2097bb333d935c08ade4b579b
Messidt <piotrekksz@gmail.com>
2017-08-31 14:28:46 +0200

    Added styles to index.php.

--- index.php
+++ index.php
@@ -17,26 +17,31 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/style.css">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
     <title>The Farmers</title>
 </head>
 <body>
-   <h1>Please log in</h1>
-   <form action="login.php" method='post'>
-       <label for="login">User name:</label>
-       <input type="text" id='login' name='login'>
-       <label for="password">Password:</label>
-       <input type="password" id='password' name='password'>
-       <input type="submit" value='Log In'>
-   </form>
+   <div class='container'>
+       <h1>Please Log In</h1>
+       <form action="login.php" method='post'>
+           <label for="login">User name:</label>
+           <input type="text" id='login' name='login' placeholder='Username'>
+           <label for="password">Password:</label>
+           <input type="password" id='password' name='password' placeholder='Password'>
+           <input type="submit" value='Enter'>
+       </form>
+       <?PHP
+    
+        // If login or password is incorrect, warning message is shown
+        if(isset($_SESSION['error'])) {
+            echo "<p>".$_SESSION['error']."</p>";
+        }
+    
+       ?>
+   </div>
    
-   <?PHP
-    
-    // If login or password is incorrect, warning message is shown
-    if(isset($_SESSION['error'])) {
-        echo $_SESSION['error'];
-    }
-    
-    ?>
+   
     
 </body>
 </html>
